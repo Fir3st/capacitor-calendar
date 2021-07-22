66676fc8b98934d8afa5abeb9f943b9112130875
Vladimír Fojtík <fojtik@net-service.cz>
2021-07-22 13:55:58 +0200

    defaultCalendar for android

--- android/src/main/java/cz/firest/calendar/Calendar.java
+++ android/src/main/java/cz/firest/calendar/Calendar.java
@@ -4,10 +4,12 @@ public class Calendar {
     String id;
     String name;
     String displayName;
+    boolean defaultCalendar;
 
-    public Calendar(String id, String name, String displayName) {
+    public Calendar(String id, String name, String displayName, boolean defaultCalendar) {
         this.id = id;
         this.name = name;
         this.displayName = displayName;
+        this.defaultCalendar = defaultCalendar;
     }
 }
